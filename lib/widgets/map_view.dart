252b68f040bc0c9c791988476fbf7d0b09bae629
CyberBismo <l.e.b@outlook.com>
2026-03-29 01:35:25 +0100

    Update map_view.dart

--- lib/widgets/map_view.dart
+++ lib/widgets/map_view.dart
@@ -394,7 +394,7 @@ class _MapViewState extends State<MapView>
               borderRadius:
                   const BorderRadius.vertical(top: Radius.circular(20)),
             ),
-            padding: EdgeInsets.fromLTRB(20, 12, 20, MediaQuery.of(context).padding.bottom + 32),
+            padding: EdgeInsets.fromLTRB(20, 12, 20, MediaQuery.of(context).padding.bottom + 14),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
